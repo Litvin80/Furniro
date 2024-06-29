95b9184cdf0451b7255d475c721ad13460e25634
Andrii Litvinchuk <litvinchyk99@gmail.com>
2024-06-29 18:23:49 +0300

    Adaptive section 'our-product' is done

--- vue-furniro/src/components/ProductCard.vue
+++ vue-furniro/src/components/ProductCard.vue
@@ -20,12 +20,25 @@
     // Посилання на елементи
     const buttonRef = ref(null);
     const imageContainerRef = ref(null);
+    const actionCardItemsRef = ref(null);
+    const productCardContentRef = ref(null);
+    const actionLikeRef = ref(null);
 
     // Якщо пристрій з тачскріном, переміщуємо кнопку в контейнер зображення
     onMounted(() => {
-        if (isTouch && buttonRef.value && imageContainerRef.value) {
-            imageContainerRef.value.appendChild(buttonRef.value);
-            buttonRef.value.classList.add('button-on-mobile');
+        if (isTouch) {
+            if (buttonRef.value && imageContainerRef.value) {
+                imageContainerRef.value.appendChild(buttonRef.value);
+                buttonRef.value.classList.add('button-on-mobile');
+            }
+            if (actionCardItemsRef.value && productCardContentRef.value) {
+                productCardContentRef.value.appendChild(actionCardItemsRef.value);
+                actionCardItemsRef.value.classList.add('action-on-mobile');
+            }
+            if (actionLikeRef.value && imageContainerRef.value) {
+                imageContainerRef.value.appendChild(actionLikeRef.value);
+                actionLikeRef.value.classList.add('like-on-mobile');
+            }
         }
     });
     
@@ -33,15 +46,15 @@
 
 <template>
     <div class="our-products__card product-card">
-        <div class="product-card__top">
-            <div class="product-card__image" ref="imageContainerRef">
+        <div class="product-card__top" ref="imageContainerRef">
+            <a href="#" class="product-card__image">
                 <img :src="imageUrl" alt="product">
-            </div>
+            </a>
         </div>
         <div class="product-card__status status-card__discounts" v-if="isDiscounts">{{discounts}}</div>
         <div class="product-card__status status-card__new" v-if="isNew">New</div>
-        <div class="product-card__content">
-            <h3 class="product-card__title">{{title}}</h3>
+        <div class="product-card__content" ref="productCardContentRef">
+            <a href="#" class="product-card__title">{{title}}</a>
             <p class="product-card__description">{{desc}}</p>
             <div class="product-card__price">
                 <span>{{price}}$</span>
@@ -49,13 +62,13 @@
             </div>
         </div>
         <a href="#" class="product-card__link"></a>
-        <div class="product-card__action action-card" :class="{ 'action-card-on-mobile': isTouch }">
+        <div class="product-card__action action-card">
             <div class="action-card__body">
-                <button ref="buttonRef" class="action-card__button button">Add to cart</button>
-                <div class="action-card__items">
-                <div class="action-card__item icon-share">Share</div>
-                <div class="action-card__item icon-compare">Compare</div>
-                <div class="action-card__item icon-heart">Like</div>
+                <button type="button" ref="buttonRef" class="action-card__button button">Add to cart</button>
+                <div class="action-card__items" ref="actionCardItemsRef">
+                    <button type="button" class="action-card__item icon-share">Share</button>
+                    <button type="button" class="action-card__item icon-compare">Compare</button>
+                    <button type="button" class="action-card__item icon-heart" ref="actionLikeRef">Like</button>
                 </div>
             </div>
         </div>
